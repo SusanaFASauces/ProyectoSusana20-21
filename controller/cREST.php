1be7597a68ce5f8ae9db67bf26151fb52b12a5c5
FabiAnton <FabiAnton@DESKTOP-97PP51H>
2021-01-30 00:21:44 +0100

    Solución de errores - Solcionado un error que hacía que no se mostrara el título ni el poster de la película por defecto.

--- controller/cREST.php
+++ controller/cREST.php
@@ -20,20 +20,17 @@ if($_REQUEST['servicios']=="omdb"){ //si se ha seleccionado el servicio omdb
         //llamamos al servicio y le pasamos el título introducido por el usuario
         $aServicioOMDb = REST::sevicioOMDb($titulo);
         if($aServicioOMDb['Response']=="False") { //si el servicio nos dice que la película no existe
-            //guardamos el mensaje y la imagen de error para pasárselos a la vista
-            $tituloEnCurso = "ERROR: ".$aServicioOMDb['Error'];
-            $imagenEnCurso = "webroot/icons/sorry.jpg";
-        }
-        else { //si se ha encontrado la película
-            //guardamos el título y la imagen para pasárselos a la vista
-            $tituloEnCurso = $aServicioOMDb['Title'];
-            $imagenEnCurso = $aServicioOMDb['Poster'];
+            //guardamos el mensaje de error
+            $error = $aServicioOMDb['Error'];
         }
     }
     else { //si no
         //llamamos al servicio y le pasamos la película por defecto
         $aServicioOMDb = REST::sevicioOMDb("The+lion+king");
     }
+    //guardamos el título y la imagen para pasárselos a la vista
+    $tituloEnCurso = $aServicioOMDb['Title'];
+    $imagenEnCurso = $aServicioOMDb['Poster'];
 }
 else { //si no
     $apodSelected = "selected"; //marcamos como seleccionado el servicio por defecto (apod)
@@ -49,5 +46,10 @@ else { //si no
     }
 }
 
+if(isset($error)) { //si ha ocurrido algún error
+    //guardamos el mensaje y la imagen de error para pasárselos a la vista
+    $tituloEnCurso = "ERROR: ".$error;
+    $imagenEnCurso = "webroot/icons/sorry.jpg";
+}
 $vistaEnCurso = $aVistas['rest']; //variable que contiene la vista que va a ejecutarse
 require_once $aVistas['layout']; //llamamos al layout
