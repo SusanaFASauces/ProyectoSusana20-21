beb11f3b94d66e2191afae3fdb250fb695c1b759
FabiAnton <FabiAnton@DESKTOP-97PP51H>
2021-02-07 17:14:52 +0100

    Implementación de servicio REST propio - Creado un servicio rest 'conversor', que permite convertir cifras entre kilogramos, gramos y miligramos. - Añadida función convertir en la clase REST. - Añadida opción de servicio 'Conversor de Unidades'. - El nuevo servicio lanza un error 400, si no recibe los parámetrso necesarios

--- view/layout.php
+++ view/layout.php
@@ -15,7 +15,7 @@
                 /**
                  * @author Susana Fabián Antón
                  * @since 20/01/2021
-                 * @version 27/01/2021
+                 * @version 07/02/2021
                  */
 
                 require_once $vistaEnCurso; //incluimos la vista que corresponda
@@ -27,7 +27,7 @@
                 <a href="https://daw208.ieslossauces.es/" target="_blank"><img src="webroot/icons/webAutor.png" alt="webAutor"></a> 
             </div>
             <address>Contacta conmigo en: susana.fabant@educa.jcyl.es</address>
-            <p>- 30 de Enero 2021 -</p>
+            <p>- 7 de Febrero 2021 -</p>
             <div class="iconos-dcha">
                 <a href="doc/phpdoc/index.html" target="_blank"><img src="webroot/icons/doc.png" alt="documentacion"></a>
                 <a href="https://github.com/SusanaFASauces/ProyectoSusana20-21" target="_blank"><img src="webroot/icons/github.png" alt="github"></a>
